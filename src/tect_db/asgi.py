e84a63d9405208d520d630b6704444a4eb47154f
Kate <bbkisik@gmail.com>
2021-01-08 14:23:40 +0300

    1st commit

---
+++ src/tect_db/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tect_db project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tect_db.settings')
+
+application = get_asgi_application()
